31295d9faba0bdd82054de5cda59c9769ea844fe
MehrshadNr <87928825+MehrshadNr@users.noreply.github.com>
2023-11-09 11:57:50 +0330

    2
    start system And fix Bug

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -7,13 +7,11 @@ public class PlayerMove : MonoBehaviour
         public Transform[] grid;
         public GameObject player;
         public int value;
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         player.transform.position = grid[value].transform.position;
--- Assets/dice.cs
+++ Assets/dice.cs
@@ -19,6 +19,23 @@ public class dice : MonoBehaviour
     {
         randomNumber = Random.Range(1, 7);
         Debug.Log(randomNumber);
-        playerMove.value += randomNumber;
+        if (randomNumber > 100 - playerMove.value)
+        {
+            Debug.Log("erorr");
+        }
+        else
+        {
+            if (playerMove.value == 0 & randomNumber == 6)
+            {
+                playerMove.value += 1;
+            }
+            else
+            {
+                if(playerMove.value > 0) 
+                {
+                    playerMove.value += randomNumber;
+                }
+            }
+        }
     }
 }
